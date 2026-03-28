c23a716a2db13a54060775dfdc340da22edc2d5b
ikuratakashi <ikura1108@icloud.com>
2026-03-28 22:27:35 +0900

    修正

--- clsDraw.py
+++ clsDraw.py
@@ -16,12 +16,12 @@ class clsDraw:
     def addLine(self, x1, y1, x2, y2, color):
         self.list.append(clsLineData(x1, y1, x2, y2, color))
 
-    def draw(self, DrawMode : DrawMode,offsetX = 0, offsetY = 0):
+    def draw(self, pDrawMode : DrawMode,offsetX = 0, offsetY = 0):
         for line in self.list:
-            if DrawMode == DrawMode.Draw:
+            if pDrawMode == DrawMode.Draw:
                 # 描画モード
                 self.display.line(line.x1 + offsetX, line.y1 + offsetY, line.x2 + offsetX, line.y2 + offsetY, 1)
-            elif DrawMode == DrawMode.Erase:
+            elif pDrawMode == DrawMode.Erase:
                 # 消去モード
                 self.display.line(line.x1 + offsetX, line.y1 + offsetY, line.x2 + offsetX, line.y2 + offsetY, 0)
         self.display.show()
