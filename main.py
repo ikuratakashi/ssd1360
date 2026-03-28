74858f1682dee53845aeb3db1dda4d689d66a814
ikuratakashi <ikura1108@icloud.com>
2026-03-28 15:12:32 +0900

    a

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ display = SSD1306_I2C(128, 64, i2c, addr=0x3C)
 display.fill(0)  # 画面をクリア
 # 横線 (x0, y0, x1, y1)
 display.line(0, 10, 127, 10, 1)
+display.line(0, 12, 120, 12, 1)
 # 縦線
 display.line(64, 0, 64, 63, 1)
+display.line(62, 0, 62, 60, 1)
 # 斜め線
-display.line(0, 0, 127, 63, 1)
+#display.line(0, 0, 127, 63, 1)
+
 display.show()
 
 # 画面を塗りつぶす
