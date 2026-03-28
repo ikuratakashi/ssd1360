eb7c92e142e1d2eaf9bfb98480a815a18aa48d02
ikuratakashi <ikura1108@icloud.com>
2026-03-28 15:08:08 +0900

    a

--- main.py
+++ main.py
@@ -7,10 +7,19 @@ from adafruit_ssd1306 import SSD1306_I2C
 i2c = busio.I2C(board.SCL, board.SDA)
 display = SSD1306_I2C(128, 64, i2c, addr=0x3C)
 
-# 画面を塗りつぶす
-display.fill(1)
+display.fill(0)  # 画面をクリア
+# 横線 (x0, y0, x1, y1)
+display.line(0, 10, 127, 10, 1)
+# 縦線
+display.line(64, 0, 64, 63, 1)
+# 斜め線
+display.line(0, 0, 127, 63, 1)
 display.show()
 
+# 画面を塗りつぶす
+#display.fill(1)
+#display.show()
+
 # 画面をクリアする
-display.fill(0)
-display.show()
+#display.fill(0)
+#display.show()
